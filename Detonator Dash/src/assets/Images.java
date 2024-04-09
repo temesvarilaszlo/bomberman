d2b0e7a3fcf57570f789f62e45a1fa9bc1c52b09
temesvarilaszlo <tlaszlo2568@gmail.com>
2024-04-09 17:59:56 +0200

    Added images and updated the image loader

--- Detonator Dash/src/assets/Images.java
+++ Detonator Dash/src/assets/Images.java
@@ -9,6 +9,7 @@ import java.io.IOException;
  * @author tlasz
  */
 public class Images {
+    // sprites
     public static Image bombImg;
     public static Image boxImg;
     public static Image fireImg;
@@ -16,12 +17,32 @@ public class Images {
     public static Image wallImg;
     public static Image whiteImg;
     
+    // drops
+    public static Image detonatorImg;
+    public static Image ghostImg;
+    public static Image invincibilityImg;
+    public static Image obstacleImg;
+    public static Image plusBombImg;
+    public static Image plusBombRangeImg;
+    public static Image skateImg;
+    
+    
     public Images() throws IOException{
+        // sprites
         bombImg = loadImage("assets/bomb.png");
         boxImg = loadImage("assets/box.png");
         fireImg = loadImage("assets/fire.png");
         pathImg = loadImage("assets/path.png");
         wallImg = loadImage("assets/wall.png");
         whiteImg = loadImage("assets/white.png");
+        
+        // drops
+        detonatorImg = loadImage("assets/detonator.png");
+        ghostImg = loadImage("assets/ghost.png");
+        invincibilityImg = loadImage("assets/invincibility.png");
+        obstacleImg = loadImage("assets/obstacle.png");
+        plusBombImg = loadImage("assets/plusbomb.png");
+        plusBombRangeImg = loadImage("assets/plusbombrange.png");
+        skateImg = loadImage("assets/rollerskate.png");
     }
 }
